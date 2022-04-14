6e68fa6073e239d4bd1c7de276a1b6230c07b728
Sinisa Dakovic <sinisa.dakovicc@gmail.com>
2022-04-14 15:52:36 +0200

    Added BrowserRouter and QueryClient

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Login from './components/login/Login'
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
 
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
     <Login/>
-    </>
+    </QueryClientProvider>
   );
 }
 
